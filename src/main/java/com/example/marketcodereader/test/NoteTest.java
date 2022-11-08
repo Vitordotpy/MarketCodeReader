1c36974a6e5d04b254c3f1f650e1145d0231e746
athena272 <guilhermera272@gmail.com>
2022-11-07 22:12:15 -0300

    Create NoteTest.java

---
+++ src/main/java/com/example/marketcodereader/test/NoteTest.java
@@ -0,0 +1,31 @@
+package com.example.marketcodereader.test;
+
+import com.example.marketcodereader.models.Cart;
+import com.example.marketcodereader.models.Client;
+import com.example.marketcodereader.models.Note;
+import com.example.marketcodereader.models.Product;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoteTest {
+
+    @Test
+    public void print() {
+        Client client = new Client("Alyson", "XXX.XXX.XXX-XX");
+
+        Product product1 = new Product((long) 4, "carne", 37.99);
+
+        Cart cart = new Cart();
+        cart.addProduct(product1);
+
+        Note note = new Note(client, cart);
+        Assert.assertEquals(
+                "Nome: Alyson\n" +
+                        "CPF/CNPJ: XXX.XXX.XXX-XX\n" +
+                        "Produtos: \n" +
+                        "carne 37.99\n",
+                note.print());
+    }
+}
